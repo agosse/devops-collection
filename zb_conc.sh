fc6ab3e11548cdbcc023a546138f2b3902824762
Alex Gosse <agosse@devbox-agosse.local>
2013-03-21 05:52:02 -0700

    Fixed getopt problems.

--- zb_conc.sh
+++ zb_conc.sh
@@ -2,8 +2,8 @@
 #set -x
 
 # Zeusbench Options
-ZB_CMD='/opt/riverbed/admin/bin/zeusbench'
-#ZB_CMD='/usr/local/zeus/admin/bin/zeusbench'
+#ZB_CMD='/opt/riverbed/admin/bin/zeusbench'
+ZB_CMD='/usr/local/zeus/admin/bin/zeusbench'
 
 # The length of time the test runs for.
 RUNTIME=30
@@ -48,36 +48,38 @@ echo ""
 }
 
 # Regex pattern for a URL.
-URL_RE="^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-‌​\.\?\,\'\/\\\+&amp;%\$#_]*)?$"
+#URL_RE="^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-‌​\.\?\,\'\/\\\+&amp;%\$#_]*)?$"
 
-# Multiple SSH Hosts or URLs?
-# New array: ARRAY=()
-# Array append: ARRAY+=('foo')
-# Array dereference (the whole thing): "${ARRAY[@]}"
+params=`getopt -o "r:,i:,l:,b:,u:,h:,s:" \
+-l "runtime:,idle-conc:,idle-linger:,busy-conc:,ssh-user:,ssh-host:,sut-url:" \
+-n "zb_conc.sh" -- "$@"`
 
-params=`getopt -o r:,i:,l:,b:,u:,h:,s: -l runtime:,idle-conc:,idle-linger:,busy-conc:,ssh-user:,ssh-host:,sut-url: -- "$@"`
+if [ $? != 0 ]
+then
+    exit 1
+fi
 
 eval set -- "$params"
 while true; do
     case "$1" in
-        --runtime ) RUNTIME="$2"; shift 2 ;;
-        --idle-conc ) IDLE_CONC="$2"; shift 2 ;;
-        --idle-linger ) IDLE_LINGER="$2"; shift 2 ;;
-        --busy-conc ) BUSY_CONC="$2"; shift 2 ;;
-        --ssh-user ) SSH_USER="$2"; shift 2 ;;
-        --ssh-host ) SSH_HOSTS="$2"; shift 2 ;;
-        --sut-url ) SUT_URLS+=("$2"); shift 2 ;;
+        --runtime|-r ) RUNTIME="$2"; shift 2 ;;
+        --idle-conc|-i ) IDLE_CONC="$2"; shift 2 ;;
+        --idle-linger|-l ) IDLE_LINGER="$2"; shift 2 ;;
+        --busy-conc|-b ) BUSY_CONC="$2"; shift 2 ;;
+        --ssh-user|-u ) SSH_USER="$2"; shift 2 ;;
+        --ssh-host|-h ) SSH_HOSTS+=("$2"); shift 2 ;;
+        --sut-url|-s ) SUT_URLS+=("$2"); shift 2 ;;
         -- ) shift; break ;;
         * ) break ;;
     esac
 done
 
 echo "Runtime: ${RUNTIME}"
-
 echo "Hosts: ${SSH_HOSTS[@]}"
+
 if [ ${#SSH_HOSTS[@]} -eq 0 ]; then
 	echo ""
-	echo "ERROR: No client machines specified.  See --ssh-hosts."
+	echo "ERROR: No client machines specified.  See --ssh-host."
 	usage
 	exit 1
 fi
@@ -88,7 +90,6 @@ if [ ${#SUT_URLS[@]} -eq 0 ]; then
 	usage
 	exit 1
 fi
-
 
 # The date today
 DATE_STR=`date +%F.%H.%M.%S`
@@ -111,8 +112,11 @@ ZB_COMMON="${ZB_CMD}"
 ZB_COMMON="${ZB_COMMON} -k"
 ZB_COMMON="${ZB_COMMON} -t ${RUNTIME}"
 
+# Build a list of forked processes
+IDLE_PIDS=()
+BUSY_PIDS=()
 
-for SSH_HOST in SSH_HOSTS; do
+for SSH_HOST in ${SSH_HOSTS[@]}; do
 
 	SSH_CMD="/usr/bin/ssh -l ${SSH_USER} ${SSH_HOST}"
 
@@ -140,7 +144,7 @@ for SSH_HOST in SSH_HOSTS; do
 	echo "Starting idle connections..."
 	${IDLE} & IDLE_PIDS+=$! # Store the PIDs...
 	echo "Starting busy connections..."
-	${BUSY} & BUSY_PID+=$!
+	${BUSY} & BUSY_PIDS+=$!
 
 	# Wait for each process to return.
 	#wait "${IDLE_PID}"
