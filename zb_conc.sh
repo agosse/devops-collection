ca8309dc0c7185fe6400b5fe17f0db19471f8767
Alex Gosse <gosse.alex@gmail.com>
2013-03-21 12:18:34 +0000

    Getopt seems not to work on OSX - for infuriating.

--- zb_conc.sh
+++ zb_conc.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#set -x
+
+# Zeusbench Options
+ZB_CMD='/opt/riverbed/admin/bin/zeusbench'
+#ZB_CMD='/usr/local/zeus/admin/bin/zeusbench'
 
 # The length of time the test runs for.
 RUNTIME=30
@@ -17,7 +22,12 @@ BUSY_CONC=1000
 
 # SSH Options
 SSH_USER='root'
-SSH_HOST='testbox'
+
+# Array of SSH Hosts (client)
+SSH_HOSTS=()
+
+# Array of SUT URLs
+SUT_URL=()
 
 function usage {
 echo ""
@@ -27,20 +37,27 @@ echo "Usage: zb_conc.sh [options]"
 echo ""
 echo "Options:"
 echo ""
-echo "  --runtime=,-r       Length of time to run for."
-echo "  --idle-conc=,-i     Number of concurrent idle connections."
-echo "  --idle-linger=,-l   Length of time between idle GETs."
-echo "  --busy-conc=,-b     Number of concurrent busy connections."
-echo "  --ssh-user=,-u      SSH user name."
-echo "  --ssh-host=,-h      SSH host \(where you want to run zeusbench\)."
-echo "  --sut-url=,-s       The URL of the system under test."
+echo "  --runtime=,-r      Length of time to run for (int | \"f\")."
+echo "  --idle-conc=,-i    Number of concurrent idle connections."
+echo "  --idle-linger=,-l  Length of time between idle GETs."
+echo "  --busy-conc=,-b    Number of concurrent busy connections."
+echo "  --ssh-user=,-u     SSH user name."
+echo "  --ssh-host=,-h     SSH host(s) - use at least once."
+echo "  --sut-url=,-s      The URL of the system(s) under test - use at least once."
+echo ""
 }
 
-params=`getopt -o r:,i:,l:,b:,u:,h:,s: \
--l runtime:,idle-conc:,idle-linger:,busy-conc:,ssh-user:,ssh-host:,sut-url: \
--- "$@"`
+# Regex pattern for a URL.
+URL_RE="^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-‌​\.\?\,\'\/\\\+&amp;%\$#_]*)?$"
 
-eval set -- "${params}"
+# Multiple SSH Hosts or URLs?
+# New array: ARRAY=()
+# Array append: ARRAY+=('foo')
+# Array dereference (the whole thing): "${ARRAY[@]}"
+
+params=`getopt -o r:,i:,l:,b:,u:,h:,s: -l runtime:,idle-conc:,idle-linger:,busy-conc:,ssh-user:,ssh-host:,sut-url: -- "$@"`
+
+eval set -- "$params"
 while true; do
     case "$1" in
         --runtime ) RUNTIME="$2"; shift 2 ;;
@@ -48,20 +65,33 @@ while true; do
         --idle-linger ) IDLE_LINGER="$2"; shift 2 ;;
         --busy-conc ) BUSY_CONC="$2"; shift 2 ;;
         --ssh-user ) SSH_USER="$2"; shift 2 ;;
-        --ssh-host ) SSH_HOST="$2"; shift 2 ;;
-        --sut-url ) ="$2"; shift 2 ;;
+        --ssh-host ) SSH_HOSTS="$2"; shift 2 ;;
+        --sut-url ) SUT_URLS+=("$2"); shift 2 ;;
         -- ) shift; break ;;
         * ) break ;;
     esac
 done
 
-SSH_CMD="/usr/bin/ssh -l ${SSH_USER} ${SSH_HOST}"
+echo "Runtime: ${RUNTIME}"
 
-# The Date
+echo "Hosts: ${SSH_HOSTS[@]}"
+if [ ${#SSH_HOSTS[@]} -eq 0 ]; then
+	echo ""
+	echo "ERROR: No client machines specified.  See --ssh-hosts."
+	usage
+	exit 1
+fi
+
+if [ ${#SUT_URLS[@]} -eq 0 ]; then
+	echo ""
+	echo "ERROR: No SUT URLs specified. See --sut-urls."
+	usage
+	exit 1
+fi
+
+
+# The date today
 DATE_STR=`date +%F.%H.%M.%S`
-
-# System Under Test URL
-SUT_URL='http://localhost/'
 
 # PERF Options
 PERF_TIMEOUT="$((${TIMEOUT}-1))"
@@ -69,17 +99,11 @@ PERF_OUT="-o perf.data.poll.${IPS}.${DATE_STR}"
 PERF_CMD="perf record ${PERF_OUT} -a -g -e cpu-cycles sleep ${PERF_TIMEOUT}"
 
 # TIME_WAIT Command
-#TW_CMD="${SSH_CMD}
-#while [ \"\$tw\" != \"0\" ];\
-#do\
-#tw=\`grep tw /proc/net/sockstat |\
-#cut -d \" \" -f 7`;\
-#echo \$tw;\
-#sleep 1;
-
-# Zeusbench Options
-ZB_CMD='/opt/riverbed/admin/bin/zeusbench'
-#ZB_CMD='/usr/local/zeus/admin/bin/zeusbench'
+TW_CMD="${SSH_CMD}
+while [ \"\$tw\" != \"0\" ]; do
+	tw=\`grep tw /proc/net/sockstat | cut -d \" \" -f 7\`;
+	echo \$tw;
+done; sleep 1"
 
 # Options common to both zeusbenches
 ZB_COMMON="${ZB_CMD}"
@@ -87,35 +111,40 @@ ZB_COMMON="${ZB_CMD}"
 ZB_COMMON="${ZB_COMMON} -k"
 ZB_COMMON="${ZB_COMMON} -t ${RUNTIME}"
 
-# Options specific to the idle zeusbench
-ZB_IDLE="${ZB_COMMON}"
-ZB_IDLE="${ZB_IDLE} -c ${IDLE_CONC}"
-ZB_IDLE="${ZB_IDLE} -l ${IDLE_LINGER}"
 
-# Options specific to the busy zeusbench
-ZB_BUSY="${ZB_COMMON}"
-ZB_BUSY="${ZB_BUSY} -c ${BUSY_CONC}"
+for SSH_HOST in SSH_HOSTS; do
 
-# Idle pattern:
-# zeusbench -v -k -t 5 -c 5 -l 1000 <target>
-# Produces: 5 concurrent x 5 seconds with 1s sleep between gets = 25 requests
+	SSH_CMD="/usr/bin/ssh -l ${SSH_USER} ${SSH_HOST}"
 
-# Busy pattern:
-# zeusbench -v -k -t 5 -c 10000 <target>
-# Produces: 10000 concurrent x 5 seconds with no sleep between gets = lots
+	# Idle pattern:
+	# zeusbench -v -k -t 5 -c 5 -l 1000 <target>
+	# Produces: 5 concurrent x 5 seconds with 1s sleep between gets = 25 requests
+	# Options specific to the idle zeusbench
+	ZB_IDLE="${ZB_COMMON}"
+	ZB_IDLE="${ZB_IDLE} -c ${IDLE_CONC}"
+	ZB_IDLE="${ZB_IDLE} -l ${IDLE_LINGER}"
 
-# Idle connections
-IDLE="${SSH_CMD} ${ZB_IDLE} ${SUT_URL}"
+	# Busy pattern:
+	# zeusbench -v -k -t 5 -c 10000 <target>
+	# Produces: 10000 concurrent x 5 seconds with no sleep between gets = lots
+	# Options specific to the busy zeusbench
+	ZB_BUSY="${ZB_COMMON}"
+	ZB_BUSY="${ZB_BUSY} -c ${BUSY_CONC}"
 
-# Busy connections
-BUSY="${SSH_CMD} ${ZB_BUSY} ${SUT_URL}"
+	# Idle connections
+	IDLE="${SSH_CMD} ${ZB_IDLE} ${SUT_URLS[@]}"
 
-echo "Starting idle connections..."
-${IDLE} & IDLE_PID=$! # Store the PIDs...
-echo "Starting busy connections..."
-${BUSY} & BUSY_PID=$!
+	# Busy connections
+	BUSY="${SSH_CMD} ${ZB_BUSY} ${SUT_URLS[@]}"
 
-# Wait for each process to return.
-wait "${IDLE_PID}"
-wait "${BUSY_PID}"
-echo "Done."
+	echo "Starting idle connections..."
+	${IDLE} & IDLE_PIDS+=$! # Store the PIDs...
+	echo "Starting busy connections..."
+	${BUSY} & BUSY_PID+=$!
+
+	# Wait for each process to return.
+	#wait "${IDLE_PID}"
+	#wait "${BUSY_PID}"
+	echo "Done."
+
+done
